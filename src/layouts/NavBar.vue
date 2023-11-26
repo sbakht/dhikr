cfd716b5190895b4cf4e84c90dcaf7288c5e17bf
sbakht <rotk13@gmail.com>
2023-11-26 17:05:44 -0600

    log out button when signed in

--- src/layouts/NavBar.vue
+++ src/layouts/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <q-toolbar>
-    <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
+    <q-btn flat dense round icon="menu" aria-label="Menu" />
 
     <q-toolbar-title shrink>
       HalalCompetitions
@@ -15,12 +15,16 @@
     <q-space />
 
     <q-tabs shrink no-caps>
-      <q-route-tab name="tab4" label="Login" to="/login" />
-      <q-route-tab name="tab5" label="Register" to="register" />
+      <q-route-tab v-if="!user" name="tab4" label="Login" to="/login" />
+      <q-route-tab v-if="!user" name="tab5" label="Register" to="register" />
+      <q-route-tab v-else name="tab5" label="Log out" to="logout" />
     </q-tabs>
   </q-toolbar>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import useAuthUser from 'src/composables/useAuthUser';
+import { ref, computed } from 'vue'
+
+const { user } = useAuthUser();
 </script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page class="row items-center justify-evenly">
     <div v-if="user">
-      {{ user }}
+      Hi {{ name }}!
     </div>
     <div v-else>
       <q-btn to="/login" color="accent">Login</q-btn>
@@ -10,6 +10,9 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue'
 import useAuthUser from '../composables/useAuthUser'
 const { user } = useAuthUser()
+
+const name = computed(() => user.value?.user_metadata.name)
 </script>
